32a6b4e979b14bc30b1ea848c845837fc982098e
jun <gmdwns9516@gmail.com>
2024-05-23 12:37:07 +0900

    1

--- basic/js/11_funtion.js
+++ basic/js/11_funtion.js
@@ -81,3 +81,10 @@ const printSteve = function() {
 }
 
 call(`Picasso`, printPicasso, printSteve)
+
+// 8. Arrow function
+const add = function(a, b) {
+    return a+b
+}
+const add2 = (a,b) => a+b
+console.log(add2);
